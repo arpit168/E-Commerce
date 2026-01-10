d23b27d5be81f67497ac1e6c642a2e00fe656c45
arpit168 <arpitgupta0406@gmail.com>
2026-01-10 18:17:42 +0530

    improvements

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { assets } from "../assets/assets";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -19,10 +20,10 @@ const Footer = () => {
         <div>
           <p className="text-xl font-medium mb-2.5">COMPANY</p>
           <ul className="flex flex-col gap-1 text-gray-600">
-            <li>Home</li>
-            <li>About us</li>
-            <li>Delivery</li>
-            <li>Privacy Policy</li>
+            <Link to="/">Home</Link>
+            <Link to="/about">About us</Link>
+            <Link to="/collection">Collection</Link>
+            <Link to="/contact">Contact </Link>
           </ul>
         </div>
 
